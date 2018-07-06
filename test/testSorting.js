5ca01e9ae5292a1941df4ca259c5cc051a3beed4
i338008 <rashmi.r02@sap.com>
2018-07-06 10:43:14 +0530

    testcase for Insertion sort

--- test/testSorting.js
+++ test/testSorting.js
@@ -7,9 +7,18 @@ QUnit.test( "Testing Insertion Sort Choice", function( assert ) {
 	var choice=1;
 	var result=menu(choice);
 	var exp_result=[3,4,5,6,7,9];
+
+	//--------------------NOTE--------------------------------------------------------------------
+	//Comparing two JavaScript objects will always return false.
+	//Two objects are strictly equal if they refer to the same Object.
+	//You are trying to compare two objects. js objects returns true only when 
+	//both point to the same memory location. So if you want to compare, whether 
+	//the CONTENT of the objects are same. You have to first convert them to string like JSON.
+	//stringify(a) == JSON.stringify(b)
+	//--------------------------------------------------------------------------------------------
 	
-		assert.ok(result===exp_result,"Insertion Sort");
-   
+	assert.ok(JSON.stringify(exp_result) == JSON.stringify(result),"Insertion Sort");
+  
 });
 
 
